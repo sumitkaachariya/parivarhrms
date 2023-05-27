3b3154d40b83a246b738c936c29800a9d53d4888
Sumit kachariya <114276572+Jalmit@users.noreply.github.com>
2023-05-27 11:38:51 +0530

    subscripation note module

--- application/controllers/user/Subscription.php
+++ application/controllers/user/Subscription.php
@@ -118,7 +118,7 @@ class Subscription extends CI_Controller {
         
         $file_name = $data['member_user']->name.'.pdf';
         // Output the generated PDF (1 = download and 0 = preview)
-        $this->dompdf->stream($file_name, array("Attachment"=>0));
+        $this->dompdf->stream($file_name, array("Attachment"=>1));
     }
 
     public function submit_subscription(){
