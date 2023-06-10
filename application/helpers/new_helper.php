8b02f1d21259dbfdc52b1607483ca4982ee04549
Sumit kachariya <114276572+Jalmit@users.noreply.github.com>
2023-06-10 11:49:00 +0530

    add maintance mode

--- application/helpers/new_helper.php
+++ application/helpers/new_helper.php
@@ -18,4 +18,16 @@ function get_all_field($table,$where,$select,$order_name,$order_type){
 	$ci->db->where($where);
 	return $ci->db->get($table)->result();
 }
+function maintenance(){
+	$table = 'admin';
+    $ci=& get_instance();
+ 	$data = $ci->db->get($table)->row();
+	if($data){
+		if($data->maintenance == 1){
+		   return 1;	
+		}else{
+			return 0;
+		}
+	} 
+}
 ?>
